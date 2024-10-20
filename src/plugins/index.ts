159ab11bcafe12c6ac447d174633e5f893bda8a1
souzaWill <57758087+souzaWill@users.noreply.github.com>
2024-10-20 18:49:23 -0300

    feat: add i18n and some translations

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -7,12 +7,22 @@
 // Plugins
 import vuetify from './vuetify'
 import router from '../router'
+import { createI18n } from 'vue-i18n';
 
 // Types
 import type { App } from 'vue'
 
+import pt_BR from '@/i18n/pt_BR.json';
+
+const messages = { pt_BR };
+const i18n = createI18n({
+  locale: 'pt_BR',
+  messages,
+});
+
 export function registerPlugins (app: App) {
   app
+    .use(i18n)
     .use(vuetify)
     .use(router)
 }
